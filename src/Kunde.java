a38b17b211216924db27fc11a16a7a0411850904
Parzival <72410271+MilanKlaussner@users.noreply.github.com>
2021-05-01 12:47:40 +0200

    Add files via upload

---
+++ src/Kunde.java
@@ -0,0 +1,13 @@
+
+public class Kunde extends Benutzer {
+
+	public Kunde(String name, String Adresse, String Passwort, int Nummer) {
+		super(name, Adresse, Passwort, Nummer);	
+		
+	}
+
+	
+	public void 
+	
+	
+}
---
+++ src/Mitarbeiter.java
@@ -0,0 +1,15 @@
+
+public class Mitarbeiter extends Benutzer {
+
+	public Mitarbeiter(String name, String Adresse, String Passwort, int Nummer) {
+		super(name, Adresse, Passwort, Nummer);
+		
+	}
+
+
+	
+}
+
+
+
+
