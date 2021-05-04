484637da5078d201c2bf3590357c017d18786bc6
daydinn <daydinn98@gmail.com>
2021-05-05 00:34:24 +0200

    erst mal unnötig
    Das sind erst mal  zu viel und auch nicht komplett fehlerfrei

--- valueObjects/Cart.java
+++ valueObjects/Cart.java
@@ -18,28 +18,7 @@ this.iList =iList;
 
 
 
-//Fügt einen Artikel den ck hinzu. Es wird geprüft ob ein Artikel bereits im Warenkorb ist, ist jedoch noch fehlerhaft
 
-public void addItem(int iNumber, int iAmount) { //Item  dem Cart hinzufügen
-Iterator<Item> iter = this.iList.iterator();
-tempItem t;
-while(iter.hasNext()) {
-Item i = iter.next();
-if(i.getNumber()==iNumber) {
-if(cartList.contains(new tempItem(i,iAmount))) {  //prüfen ob der Item bereits im Cart ist(fehlerhaft)
-t = cartList.get(cartList.indexOf(i));  //get gibt ein objekt aus einer Liste zurück, indexof liefert die stelle in einer Liste von einem Objekt
-System.out.println("it already exists"); //nur zum testen
-}else{
-if(i.getStock()>=iAmount) {
-cartList.add(new tempItem(i,iAmount));
-System.out.println("it doenst exist yet"); //zum testen
-}else{
-System.out.println("There are not enough items in stock");	
-}
-}
-}
-}
-}
 
 
 //löscht einen Item aus dem Cart, je nach dem wie groß die Anzahl ist
