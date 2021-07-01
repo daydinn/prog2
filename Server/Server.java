f469f33228290a04dc86fd3a6fc9f5f6bb594387
xsilvermc <57896691+xsilvermc@users.noreply.github.com>
2021-07-01 17:14:36 +0200

    sockets

---
+++ Server/Server.java
@@ -0,0 +1,26 @@
+package Server;
+
+import java.net.*;
+import java.io.*;
+
+public class Server {
+	
+	public static void main(String args[]) throws IOException {
+		System.out.println("initializing...");
+		ServerSocket serverSocket = new ServerSocket(4999);
+		Socket clientSocket = serverSocket.accept();
+		
+		System.out.println("client connected");
+		
+		InputStreamReader in = new InputStreamReader(clientSocket.getInputStream());
+		BufferedReader bf = new BufferedReader(in);
+		
+		String str = bf.readLine();
+		System.out.println("client: "+str);
+		
+		PrintWriter pr = new PrintWriter(clientSocket.getOutputStream());
+		pr.println("yes");
+		pr.flush();
+	}
+	
+}
